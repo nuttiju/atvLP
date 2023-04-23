0379ecc4afa5ea0f542fc47c08ba78359212412d
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 18:50:03 -0300

    Create ex30.py

---
+++ ex30.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+A = int(input("Digite o primeiro número: "))
+B = int(input("Digite o segungo número: "))
+
+if A % B == 0 or B % A == 0:
+    print("São Multiplos")
+else:
+    print("Nao são Multiplos")
