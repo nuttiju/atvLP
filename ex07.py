d58d633f50cff35b11165705f7e64a3249782770
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 15:42:55 -0300

    Create ex07.py

---
+++ ex07.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+  N = int(input("Digite um número de 0 a 10 para calcularmos a tabuada: "))
+  for i in range (1, 11):
+  tabuada = N*i
+  print("{} x {} = {}".format(n, i, tabuada) 
