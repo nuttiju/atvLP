f2e4df47f9a616dbd852dff55ec5004008661132
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 15:55:02 -0300

    Create ex09.py

---
+++ ex09.py
@@ -0,0 +1,7 @@
+  if __name__ == '__main__':
+    A = int(input('Digite um valor inicial: '))
+    R = int(input('Digite o valor da razão: '))
+  
+    for i in range(1, 11):
+       valor = A * (R**i)
+        print(valor)
