e92a35f53415315db29ee40031e1f4eb34739458
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 15:33:39 -0300

    Create ex05.py

---
+++ ex05.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+  Npar = 0
+  Nimpar = 0
+  Soma = 0
+  Spar = 0 
+  
+  while numero > 0:
+    numero = int(input("Digite um número (Caso queira sair digite 0): "))
+    if numero % 2 == 0 
+    Npar += 1 
+    Spar += numero 
+    else
+    Nimpar += 1
+    
+    Soma += numero
+    
+   Mpares = Spar / Npar 
+   Mgeral = Soma / (Npar + Nimpar) 
+    
+    print ("Quantidade de números pares: ", Npar)
+    print ("Quantidade de números impares: ", Nimpar)
+    print ("Média dos valores pares: ", Mpares)
+    print ("Média de todos os valores: ", Mgeral)
+    
+    
+      
