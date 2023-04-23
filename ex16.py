afc84239218b8a6d1fa7e7440102a53164b3a87b
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 16:40:51 -0300

    Create ex16.py

---
+++ ex16.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+ A = float(input("Digite a primeira nota: "))
+ B = float(input("Digite a segunda nota: "))
+ C = float(input("Digite a terceira nota: "))
+  media = ((A*2) + (B*3) + (C*5)) / 10
+  prin("MÉDIA + {} ".format(media)) 
