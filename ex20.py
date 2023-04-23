5b60a2753c8563252d333ee267cb66d6a548e6b3
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 17:19:59 -0300

    Create ex20.py

---
+++ ex20.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+cod1 = int(input("Digite o código da peça 1: "))
+peca1 = int(input("Digite a quantidade da peça 1: "))
+valor_peca1 = float(input("Digite o valor da peça 1: "))
+
+cod2 = int(input("Digite o código da peça 2: "))
+peca2 = int(input("Digite a quantidade da peça 2: "))
+valor_peca2 = float(input("Digite o valor da peça 2: "))
+
+total = (peca1 * valor_peca1) + (peca2 * valor_peca2)
+
+print("O valor total a ser pago é R$ {}".format(total))
