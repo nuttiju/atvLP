3de6115b966dac4616aa7b955ea63891cebbff56
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 16:11:57 -0300

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+ A = 0
+ B = 0
+ print("Vamos descobrir o valor de X")
+ A = int(input("Digite o primeiro número: ")
+ B = int(input("Digite o segundo número: ")
+         X = A + B
+     print("X + {}".format(X))
+         
