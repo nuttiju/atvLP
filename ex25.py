adeba57f7ebabe54bd69891dcefa216cfd685957
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 17:56:18 -0300

    Create ex25.py

---
+++ ex25.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+
+x1 = float(input("Digite a primeira coordenada do primeiro ponto: ")
+y1 = float(input("Digite a segunda coordenada do primeiro ponto: ")
+x2 = float(input("Digite a primeira coordenada do segundo ponto: ")
+y2 = float(input("Digite a segunda coordenada do segundo ponto: ")
+
+distancia = ((x2 - x1) ** 2 + (y2 - y1) ** 2)**0.5
+
+print("{}".format(distancia))
