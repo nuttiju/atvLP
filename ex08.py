da253cc838647c4a1c589a60d276c824e690a876
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 15:51:40 -0300

    Create ex08.py

---
+++ ex08.py
@@ -0,0 +1,8 @@
+  if __name__ == '__main__':
+    A = int(input('Digite um valor inicial: '))
+    R = int(input('Digite o valor da razão: '))
+  
+    for i in range(1, 11):
+       valor = A + i*R
+        print(valor)
+   
