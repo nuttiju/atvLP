ebf0657d16ccd2841204e4c90b6205ef93810bd3
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 16:34:42 -0300

    Create ex15.py

---
+++ ex15.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+ Nota1 = float(input("Digite a primeira nota: ")
+ Nota2 = float(input("Digite a segunda nota: ")
+      media = ((Nota1 * 3.5) = (Nota2 * 7.5)) / 11
+      print("MÉDIA = {} ".format (media))
