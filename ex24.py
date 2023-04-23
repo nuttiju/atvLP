0786a3486fc449141d4fa8cda5594800610e6365
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 17:43:07 -0300

    Create ex24.py

---
+++ ex24.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+x = int(input("Digite a distancia total percorrida em KM: ")
+y = float(input("Digite o total de combustivel gasto em litros: ")
+     km_l = x / y
+   print("{} km/l".format(km_l))
+                    
