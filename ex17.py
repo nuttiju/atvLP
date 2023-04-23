26d4890df1b63aae983b4885355846258470014b
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 16:45:21 -0300

    Create ex17.py

---
+++ ex17.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+  A = int(input("Digite o primeiro número: ")
+  B = int(input("Digite o segundo número: ")
+  C = int(input("Digite o terceiro número: ")
+  D = int(input("Digite o quarto número: ")
+         DIF = A*B - C*D
+          print ("DIFERENÇA = {} ".format(DIF) 
