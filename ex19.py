62b2878c3d33411f3ab35d732c7894af3c6e1061
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 17:01:05 -0300

    Create ex19.py

---
+++ ex19.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+  Nome = str(input("Digite o nome do vendedor: ")
+  Salario = float(input("Digite o salário fixo do vendedor: ")
+  Vendido = float(input("Digite o valor que o vendedor vendeu durante o mês: ")
+  Comissao = Vendido*0.15
+  Total = Comissao+Salario
+      print("TOTAL = R$ {} ".format(Total))
+                  
