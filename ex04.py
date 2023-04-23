738d6a6c5088fe33b18bf0dccab2af085299b504
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 15:19:50 -0300

    Create ex04.py

---
+++ ex04.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+intervalo1 = 0
+intervalo2 = 0
+intervalo3 = 0
+intervalo4 = 0 
+
+while True:
+  numero = int(input("Digite um número de 0 a 100: "))
+  if numero >=0 and numero <=25
+    intervalo1 += 1
+   elif numero >=26 and numero <=50
+    intervalo2 += 1 
+    elif numero >=51 and numero <=75 
+     intervalo3 += 1
+      elif numero >=76 and numero <=100
+      intervalo4 += 1 
+      else numero < 0 
+      break
+print ('Existe {} números no intervalo de 0 a 25'.format(intervalo1))
+print ('Existe {} números no intervalo de 26 a 50'.format(intervalo2))
+print ('Existe {} números no intervalo de 51 a 75'.format(intervalo3))
+print ('Existe {} números no intervalo de 76 a 100'.format(intervalo1))
