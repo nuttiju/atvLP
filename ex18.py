dd0cc05bfecc460f09e5039cc64703beb6fc1726
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 16:52:01 -0300

    Create ex18.py

---
+++ ex18.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+  N = int(input("Digite o número do funcionario: ")
+  H = int(input("Digite quantas horas esse funcionario trabalhou: ")
+  SHora = float(input("Digite quanto o funcionario recebe por hora trabalhada: ")
+  salario = H*SHora
+       print("NUMBER = {} ".format(N))
+       print("SALARY = R$ {} ".format(salario))
