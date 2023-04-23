f41fef80f3a2a6dc03c04a2da2c1a0bf54f46866
Júlia Nutti <82540936+nuttiju@users.noreply.github.com>
2023-04-23 13:17:56 -0300

    Create ex02.py
    2) Desenvolver um algoritmo que leia a altura de 15 pessoas. Este programa deverá calcular e
    mostrar :
    a. A menor altura do grupo;
    b. A maior altura do grupo;

---
+++ ex02.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+  menor = 0
+  maior = 0
+    for i in range (1, 16)
+      altura = float(input("Digite a altura do integrante {}:" .format (i)))
+      if altura > maior 
+        maior = altura
+      if altura < menor 
+        menor = altura 
+  print("A maior altura é {}" .format (maior))
+  print("A menor altura é {}" .format (menor)) 
+  
